e25fef391fa83e54a9097c33514869c285993ed2
cishocksr <cishockleysr@gmail.com>
2020-01-15 22:24:35 -0500

    api running

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // implement your API here
+const express = require("express");
+
+const server = express();
+
+server.listen(4000, () => {
+  console.log("=== server listening on port 4000 ===");
+});
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send("hello from the api");
+});
