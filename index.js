ddf625790e7903e1ae0c3c8f43dbab8ccd36c816
cishocksr <cishockleysr@gmail.com>
2020-01-16 02:49:06 -0500

    post is working

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// implement your API here
+const db = require("./data/db");
 const express = require("express");
 
 const server = express();
@@ -9,6 +9,32 @@ server.listen(4000, () => {
 
 server.use(express.json());
 
-server.get("/", (req, res) => {
-  res.send("hello from the api");
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ errorMessge: "The users information could not be retrieved." });
+    });
+});
+
+server.post("/api/users", (req, res) => {
+  const usersInfo = req.body;
+
+  if (usersInfo.name && usersInfo.bio) {
+    db.insert(usersInfo)
+      .then(user => {
+        res.status(201).json({ success: true, user });
+      })
+      .catch(err => {
+        res.status(500).json({ success: false, err });
+      });
+  } else {
+    res.status(400).json({
+      message: "Please provide a name and bio for the user"
+    });
+  }
 });
